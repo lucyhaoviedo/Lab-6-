a3675d754ad8fcced9af90419fb853ed0a1f0816
Lab <jpatel9@ufl.edu>
2023-10-25 16:35:52 -0400

    here is lab 6 encoder part

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+while True:
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    user_input=(int(input("Please enter an option: ")))
+    if user_input==1:
+        password_in=(input("Please enter your password to encode:"))
+        new_pass=""
+        for char in password_in:
+            if 0<= int(char)<=6:
+                encoded_char= str(int(char)+3)
+                new_pass+=encoded_char
 
+            else:
+                encoded_char=str((int(char)+3) % 10)
+                new_pass+=encoded_char
+    print("Your password has been encoded and stored!")
+    elif user_input==2:
+        pass
+    elif user_input == 3:
+        break
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
-for i in range
